b47f39b81f23a7323f976b018171146d359d49fa
pricefy-dotcom <tylerprice.it@gmail.com>
2026-07-02 16:16:46 -0400

    Updated frontend to use live Render API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -69,7 +69,7 @@ export default function App() {
         payload[key] = parseInt(form[key]);
       }
 
-      const response = await fetch('http://127.0.0.1:8000/recommend', {
+      const response = await fetch('https://learning-recommender-1xfe.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
